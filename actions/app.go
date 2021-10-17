50005dd3be2f3d62b0ff0251eaf484f6657e8f80
Ashok Raja <ashokraja.r@gmail.com>
2021-10-17 10:47:33 +0400

    feature(rebranding): Replacing default homepage with On-Call Incident home page
    - moved the routes list to /routes

--- actions/app.go
+++ actions/app.go
@@ -61,6 +61,7 @@ func App() *buffalo.App {
 
 		app.GET("/", HomeHandler)
 
+		app.GET("/routes", RoutesHandler)
 		app.GET("/healthz", HomeHealth)
 		app.Resource("/incidents", IncidentsResource{})
 		app.ServeFiles("/", assetsBox) // serve files from the public directory
